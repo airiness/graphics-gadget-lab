81f692a556199a16392091aed2edacce59eba3eb
ruisong <airiness021@gmail.com>
2025-04-08 22:09:21 +0900

    temp commit for sync(Create Pipeline State)

--- Application/DX12PipelineState.h
+++ Application/DX12PipelineState.h
@@ -1,31 +1,34 @@
 #pragma once
 namespace graphicsGadgetLab
 {
+	class DX12Device;
 	class DX12PipelineState
 	{
 	public:
-		DX12PipelineState() noexcept;
-		DX12PipelineState(const DX12PipelineState&) = delete;
-		DX12PipelineState& operator=(const DX12PipelineState&) = delete;
-		DX12PipelineState(DX12PipelineState&&) = delete;
-		DX12PipelineState& operator=(DX12PipelineState&&) = delete;
+		explicit DX12PipelineState(DX12Device* dx12Device) noexcept;
+
 		~DX12PipelineState() noexcept;
 		void Initialize() noexcept;
 		void Finalize() noexcept;
 
 
 	private:
+		DX12Device* m_DX12Device = nullptr;
 		ComPtr<ID3D12PipelineState> m_PipelineState;
 		ComPtr<ID3D12RootSignature> m_RootSignature;
 		std::vector<ComPtr<ID3DBlob>> m_ShaderBlobs;
 		std::vector<ComPtr<ID3DBlob>> m_ShaderErrors;
-		std::vector<D3D12_INPUT_ELEMENT_DESC> m_InputElements;
-		D3D12_GRAPHICS_PIPELINE_STATE_DESC m_PipelineStateDesc = {};
-		D3D12_ROOT_SIGNATURE_DESC m_RootSignatureDesc = {};
-		bool m_Initialized = false;
-
 	};
 
+	struct DX12GraphicsPipeLineStateDesc
+	{
+		D3D12_GRAPHICS_PIPELINE_STATE_DESC m_Desc = {};
+	};
+
+	struct DX12ComputePipeLineStateDesc
+	{
+		D3D12_COMPUTE_PIPELINE_STATE_DESC m_Desc = {};
+	};
 
 }
 
