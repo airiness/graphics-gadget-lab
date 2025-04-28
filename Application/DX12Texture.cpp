46f7740de4f8e71dd071004fa524c208ffbbd3ee
ruisong <ruisong@grezzo.co.jp>
2025-04-28 20:35:07 +0900

    temp commit for sync(Buffer,Texture class)

--- Application/DX12Texture.cpp
+++ Application/DX12Texture.cpp
@@ -8,7 +8,12 @@ namespace graphicsGadgetLab
 		const CD3DX12_RESOURCE_DESC& resourceDesc,
 		D3D12_RESOURCE_STATES initState,
 		std::optional<D3D12_CLEAR_VALUE> clearValue) noexcept :
-		DX12Resource(device, heapType, resourceDesc, initState, clearValue)
+		m_dx12device(device),
+
+	{
+	}
+
+	DX12Texture::DX12Texture(DX12Device* device, ComPtr<ID3D12Resource> resource) noexcept
 	{
 	}
 
