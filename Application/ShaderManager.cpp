8a10c693ad771aeadef484d7261fdb3a11663c67
ruisong <airiness021@gmail.com>
2025-09-21 21:35:13 +0900

    Making SHaderManager

--- Application/ShaderManager.cpp
+++ Application/ShaderManager.cpp
@@ -1,16 +1,60 @@
 #include "Precompiled.h"
 #include "ShaderManager.h"
-
+#include "Utility.h"
 
 namespace gglab
 {
 	ShaderManager::ShaderManager() noexcept
 	{
+		utility::ThrowIfFailed(DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(&m_DxcUtils)));
 	}
 
-	ShaderId ShaderManager::LoadCSO(const std::filesystem::path& path, ShaderStage stage) noexcept
+	ShaderId ShaderManager::LoadShader(const std::filesystem::path& path, ShaderStage stage) noexcept
 	{
-		return ShaderId();
+		// path check
+		if (!std::filesystem::exists(path))
+		{
+			GGLAB_LOG_GRAPHICS_ERROR("ShaderManager::LoadShader: File not found: {}", path.string());
+			return ShaderId();
+		}
+
+		ShaderKey key{};
+		key.m_PathHash = StringId(path.string());
+		key.m_Stage = stage;
+
+		{
+			std::shared_lock lock(m_Mutex);
+			if(auto it = m_KeyIdMap.find(key); it != m_KeyIdMap.end())
+			{
+				return it->second;
+			}
+		}
+
+		ComPtr<ShaderBlob> blob;
+		utility::ThrowIfFailed(m_DxcUtils->LoadFile(path.wstring().c_str(), nullptr, &blob));
+
+		auto timestamp = std::filesystem::last_write_time(path);
+
+		{
+			std::unique_lock lock(m_Mutex);
+			if(auto it = m_KeyIdMap.find(key); it != m_KeyIdMap.end())
+			{
+				return it->second;
+			}
+
+			ShaderRecord record{};
+			record.m_Key = key;
+			record.m_Path = path;
+			record.m_Stage = stage;
+			record.m_Blob = std::move(blob);
+			record.m_Timestamp = timestamp;
+			record.m_Hash = HashBlob(record.m_Blob.Get());
+
+			const auto shaderId = static_cast<ShaderId>(m_Records.size());
+			m_Records.push_back(std::move(record));
+			m_KeyIdMap.emplace(key, shaderId);
+			return shaderId;
+		}
 	}
 
 	D3D12_SHADER_BYTECODE ShaderManager::GetBytecode(ShaderId shaderId) const noexcept
@@ -18,7 +62,7 @@ namespace gglab
 		return D3D12_SHADER_BYTECODE();
 	}
 
-	const ComPtr<ID3DBlob>& ShaderManager::GetBlob(ShaderId shaderId) const noexcept
+	const ComPtr<ShaderBlob>& ShaderManager::GetBlob(ShaderId shaderId) const noexcept
 	{
 		// TODO: insert return statement here
 		return nullptr;
@@ -29,7 +73,7 @@ namespace gglab
 		return ShaderHash128();
 	}
 
-	uint64_t ShaderManager::GetGeneration() const noexcept
+	uint64_t ShaderManager::GetGeneration(ShaderId shaderId) const noexcept
 	{
 		return 0;
 	}
@@ -43,13 +87,29 @@ namespace gglab
 		return 0;
 	}
 
-	ShaderId ShaderManager::InsertOrGet(const ShaderKey& key, ComPtr<ID3DBlob>&& blob, ShaderStage stage) noexcept
+	ShaderId ShaderManager::InsertOrGet(const ShaderKey& key, ComPtr<ShaderBlob>&& blob, ShaderStage stage) noexcept
 	{
+		if(auto it = m_KeyIdMap.find(key); it != m_KeyIdMap.end())
+		{
+			return it->second;
+		}
+
+		ShaderRecord record{};
+		record.m_Key = key;
+		record.m_Path = key.m_PathHash.GetString();
+
+
 		return ShaderId();
 	}
 
 	bool ShaderManager::GetDxilValidatorHash(const void* data, size_t size, ShaderHash128& outHash) noexcept
 	{
+		constexpr size_t MinDxilSize = 20;
+		if(data == nullptr || size < MinDxilSize)
+		{
+			return false;
+		}
+
 		// magic number for DirectX Container
 		// https://llvm.org/docs/DirectX/DXContainer.html?utm_source=chatgpt.com#file-header
 		static const unsigned char DXBCMagicNumber[] = { 'D', 'X', 'B', 'C' };
@@ -64,20 +124,27 @@ namespace gglab
 		return true;
 	}
 
-	ShaderHash128 ShaderManager::HasBlob(ID3DBlob* blob) noexcept
+	ShaderHash128 ShaderManager::HashBlob(ShaderBlob* blob) noexcept
 	{
-		ShaderHash128 key{};
+		ShaderHash128 hash{};
 		if (!blob)
 		{
-			return key;	
+			GGLAB_LOG_GRAPHICS_WARN("ShaderManager::HashBlob: blob is null.");
+			return hash;
 		}
-
-		
 
 		const auto* ptr = static_cast<const uint8_t*>(blob->GetBufferPointer());
 		const auto size = blob->GetBufferSize();
 
-		//key.m_LowBits = FNV1a64::HashValue(ptr);
+		if(GetDxilValidatorHash(ptr, size, hash))
+		{
+			return hash;
+		}
 
+		// FNV-1a 64-bit hash
+		GGLAB_LOG_GRAPHICS_WARN("ShaderManager::HashBlob: Failed to get DXIL validator hash, fallback to FNV-1a hash.");
+		hash.m_LowBits = FNV1a64::HashBytes64(ptr, size);
+		hash.m_HighBits = FNV1a64::HashBytes64(ptr, size, 0x9ae16a3b2f90404full);
+		return hash;
 	}
 }
--- Application/ShaderManager.h
+++ Application/ShaderManager.h
@@ -8,6 +8,12 @@ namespace gglab
 {
 	GGLAB_DEFINE_TYPED_INDEX(ShaderId, uint32_t);
 
+	// DXC shader blob
+	using ShaderBlob = IDxcBlobEncoding;
+
+	// D3D shader blob
+	// using ShaderBlob = ID3DBlob;
+
 	enum class ShaderStage
 	{
 		Vertex,
@@ -39,7 +45,7 @@ namespace gglab
 		ShaderKey m_Key{};
 		std::filesystem::path m_Path{};
 		ShaderStage m_Stage = ShaderStage::Vertex;
-		ComPtr<ID3DBlob> m_Blob;
+		ComPtr<ShaderBlob> m_Blob;
 		ShaderHash128 m_Hash{};
 		std::filesystem::file_time_type m_Timestamp{};
 		uint64_t m_Generation = 1;
@@ -52,25 +58,27 @@ namespace gglab
 		GGLAB_DELETE_COPYABLE_MOVABLE(ShaderManager);
 		~ShaderManager() = default;
 
-		ShaderId LoadCSO(const std::filesystem::path& path, ShaderStage stage) noexcept;
+		ShaderId LoadShader(const std::filesystem::path& path, ShaderStage stage) noexcept;
 		D3D12_SHADER_BYTECODE GetBytecode(ShaderId shaderId) const noexcept;
-		const ComPtr<ID3DBlob>& GetBlob(ShaderId shaderId) const noexcept;
+		const ComPtr<ShaderBlob>& GetBlob(ShaderId shaderId) const noexcept;
 		ShaderHash128 GetHash(ShaderId shaderId) const noexcept;
-		uint64_t GetGeneration() const noexcept;
+		uint64_t GetGeneration(ShaderId shaderId) const noexcept;
 
 		void Preload(const std::vector<std::pair<std::filesystem::path, ShaderStage>>& shaders) noexcept;
 		int32_t ReloadChanged() noexcept;
 
 	private:
-		ShaderId InsertOrGet(const ShaderKey& key, ComPtr<ID3DBlob>&& blob, ShaderStage stage) noexcept;
+		ShaderId InsertOrGet(const ShaderKey& key, ComPtr<ShaderBlob>&& blob, ShaderStage stage) noexcept;
 
 	private:
 		static bool GetDxilValidatorHash(const void* data, size_t size, ShaderHash128& outHash) noexcept;
-		static ShaderHash128 HasBlob(ID3DBlob* blob) noexcept;
+		static ShaderHash128 HashBlob(ShaderBlob* blob) noexcept;
 
 	private:
 		mutable std::shared_mutex m_Mutex;
 		std::unordered_map<ShaderKey, ShaderId, ShaderKeyHash> m_KeyIdMap;
 		std::vector<ShaderRecord> m_Records;
+
+		ComPtr<IDxcUtils> m_DxcUtils;
 	};
 }
