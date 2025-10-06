d81e1c4ed7c689eb4a7de7edf20e23b150edece9
ruisong <airiness021@gmail.com>
2025-10-06 23:52:46 +0900

    Making Shader Compiler

--- Application/Shader.cpp
+++ Application/Shader.cpp
@@ -1,25 +1,28 @@
 #include "Precompiled.h"
 #include "Shader.h"
+#include "ShaderCompiler.h"
 
 namespace gglab
 {
-	Shader::Shader(const ShaderDesc& desc) noexcept
+	Shader::Shader(const ShaderDesc& desc) noexcept :
+		m_Desc(desc)
 	{
-	}
-
-	bool Shader::EnsureCompiled(ShaderCompiler& compiler) noexcept
-	{
-		ShaderCompileSnapshot
-
-		return false;
-	}
-
-	bool Shader::ForceRecompile(ShaderCompiler& compiler) noexcept
-	{
-		return false;
 	}
 
 	D3D12_SHADER_BYTECODE Shader::GetByteCode() const noexcept
 	{
+		GGLAB_ASSERT_MSG(IsValid(), "Shader must be compiled.");
+
+		D3D12_SHADER_BYTECODE byteCode{};
+		byteCode.pShaderBytecode = m_Artifact.m_Blob->GetBufferPointer();
+		byteCode.BytecodeLength = m_Artifact.m_Blob->GetBufferSize();
+
+		return byteCode;
+	}
+
+
+	bool Shader::EnsureCompiled(ShaderCompiler& compiler) noexcept
+	{
+		
 	}
 }
--- Application/Shader.h
+++ Application/Shader.h
@@ -43,24 +43,33 @@ namespace gglab
 		std::filesystem::path m_SourcePath;
 		ShaderStage m_Stage;
 		ShaderModel m_Model;
-		std::wstring m_Entry = L"";
+		std::wstring m_Entry;
 		std::vector<std::filesystem::path> m_IncludeDirs;
 		std::vector<ShaderDefine> m_Defines;
 		ShaderCompileFlag m_Flags;
 		std::vector<std::wstring> m_ExtraArgs;
 		std::wstring m_HlslVersion = L"2021";
-		std::wstring m_OptLevel = L"03";
-
+		std::wstring m_OptLevel = L"O3";
 	};
 
-	struct ShaderCompileSnapshot
+	struct ShaderCompileArtifact
 	{
 		std::filesystem::path m_DxilPath;
 		std::filesystem::path m_MetaPath;
 		ComPtr<ShaderBlob> m_Blob;
 		ShaderHash128 m_Hash;
-		bool m_FromCache = false;
 		std::filesystem::file_time_type m_SourceTimeStamp{};
+		bool m_FromCache = false;
+
+		void Reset() noexcept
+		{
+			m_DxilPath.clear();
+			m_MetaPath.clear();
+			m_Blob.Reset();
+			m_Hash = {};
+			m_SourceTimeStamp = {};
+			m_FromCache = false;
+		}
 	};
 
 	class ShaderCompiler;
@@ -69,22 +78,23 @@ namespace gglab
 	{
 	public:
 		explicit Shader(const ShaderDesc& desc) noexcept;
-		GGLAB_DELETE_COPYABLE_DEFAULT_MOVABLE(Shader);
+		GGLAB_DELETE_COPYABLE_MOVABLE(Shader);
 		~Shader() = default;
-
-		bool EnsureCompiled(ShaderCompiler& compiler) noexcept;
-
-		bool ForceRecompile(ShaderCompiler& compiler) noexcept;
 
 		D3D12_SHADER_BYTECODE GetByteCode() const noexcept;
 		const ShaderDesc& GetDesc() const noexcept { return m_Desc; }
-		const ShaderCompileSnapshot& GetSnapshot() const noexcept { return m_Snapshot; }
+		const ShaderCompileArtifact& GetCompileArtifact() const noexcept { return m_Artifact; }
 		uint64_t GetGeneration() const noexcept { return m_Generation; }
-		bool IsValid() const noexcept { return m_Snapshot.m_Blob != nullptr; }
+		bool IsValid() const noexcept { return m_Artifact.m_Blob != nullptr; }
+
+	private:
+		bool EnsureCompiled(ShaderCompiler& compiler) noexcept;
 
 	private:
 		ShaderDesc m_Desc;
-		ShaderCompileSnapshot m_Snapshot;
+		ShaderCompileArtifact m_Artifact;
 		uint64_t m_Generation = 0;
+
+		friend class ShaderManager;
 	};
 }
