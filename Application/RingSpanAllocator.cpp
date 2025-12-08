3229ccb85d4676261513f0cec86028aecf441691
ruisong <ruisong@grezzo.co.jp>
2025-12-08 15:33:25 +0900

    RingAllocator bug fix

--- Application/RingSpanAllocator.cpp
+++ Application/RingSpanAllocator.cpp
@@ -12,40 +12,55 @@ namespace gglab
 	{
 		if (count == 0)
 		{
-			GGLAB_LOG_WARN("RingSpanAllocator: Allocate(). count is 0, nothing allocated.");
+			GGLAB_LOG_WARN("RingSpanAllocator: Allocate failed. count is 0, nothing allocated.");
+			return {};
+		}
+
+		if (count > m_FreeCount)
+		{
+			GGLAB_LOG_WARN("RingSpanAllocator: Allocate failed. request={}, free={}",
+				count, m_FreeCount);
 			return {};
 		}
 
 		OffsetType index = 0;
 		if (m_Tail >= m_Head)
 		{
-			OffsetType right = m_Capacity - m_Tail;
-			if (count <= right - (m_Head == 0 ? 1u : 0u))
+			const OffsetType right = m_Capacity - m_Tail;
+
+			// try to allocate at [tail, capacity)
+			const OffsetType guard = (m_Head == 0 ? 1u : 0u);
+			if (count <= right - guard)
 			{
 				index = m_Tail;
-				m_Tail = (m_Tail + count) % m_Capacity;
+				m_Tail = static_cast<OffsetType>((m_Tail + count) % m_Capacity);
 				m_FreeCount -= count;
 				return IndexSpan{ .m_Index = index, .m_Count = count };
 			}
 
+			// try ao allocate at [0, head)
 			if (m_Head > 1 && count <= (m_Head - 1))
 			{
 				index = 0;
-				m_Tail = count % m_Capacity;
+				m_Tail = static_cast<OffsetType>(count % m_Capacity);
 				m_FreeCount -= count;
-				return IndexSpan{ .m_Index = 0, .m_Count = count };
+				return IndexSpan{ .m_Index = index, .m_Count = count };
 			}
 		}
 
-		const auto available = m_Head - m_Tail - 1;
+		// tail is on left of head, free range is (tail, head)
+		const auto available = static_cast<CountType>(m_Head - m_Tail - 1);
 		if (count <= available)
 		{
 			index = m_Tail;
-			m_Tail = (m_Tail + count) % m_Capacity;
+			m_Tail = static_cast<OffsetType>((m_Tail + count) % m_Capacity);
 			m_FreeCount -= count;
 			return IndexSpan{ .m_Index = index, .m_Count = count };
 		}
 
+		GGLAB_LOG_WARN(
+			"RingSpanAllocator::Allocate failed: no contiguous space. request = {}, free = {}.",
+			count, m_FreeCount);
 		return {};
 	}
 
@@ -68,13 +83,19 @@ namespace gglab
 			{
 				break;
 			}
-			if (m_Head == front.m_IndexSpan.m_Index)
-			{
-				m_Head = (front.m_IndexSpan.m_Index + front.m_IndexSpan.m_Count) % m_Capacity;
-			}
 
+			// Free completed
 			m_FreeCount += front.m_IndexSpan.m_Count;
 			m_RetireRecords.pop_front();
+
+			if (!m_RetireRecords.empty())
+			{
+				m_Head = m_RetireRecords.front().m_IndexSpan.m_Index;
+			}
+			else
+			{
+				m_Head = m_Tail;
+			}
 		}
 	}
 }
