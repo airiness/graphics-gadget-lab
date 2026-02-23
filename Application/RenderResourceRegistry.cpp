2fe43bdcf0c0599ea0bbb3c21612a1c46425640b
ruisong <airiness021@gmail.com>
2026-02-23 22:50:29 +0900

    temp

--- Application/RenderResourceRegistry.cpp
+++ Application/RenderResourceRegistry.cpp
@@ -3,8 +3,16 @@
 
 namespace gglab
 {
-	RenderResourceRegistry::RenderResourceRegistry() noexcept
+	RenderResourceRegistry::RenderResourceRegistry(const CreateInfo& createInfo) noexcept :
+		m_GpuResourceAllocator(createInfo.m_GpuResourceAllocator),
+		m_ExternalResourceRegistry(createInfo.m_ExternalResourceRegistry)
 	{
+		GGLAB_ASSERT(m_GpuResourceAllocator);
+		GGLAB_ASSERT(m_ExternalResourceRegistry);
+	}
 
+	DX12Texture* RenderResourceRegistry::GetTexture(TextureIndex index) noexcept
+	{
+		return nullptr;
 	}
 }
--- Application/RenderResourceRegistry.h
+++ Application/RenderResourceRegistry.h
@@ -1,19 +1,64 @@
 #pragma once
+#include "RGResource.h"
+#include "GraphicsTypes.h"
+#include "TypeUtils.h"
 
 namespace gglab
 {
-	enum class RenderResourceTag : uint8_t
-	{
-		IBL
-	};
+	class RGGpuResourceAllocator;
+	class RGExternalResourceRegistry;
+	class DX12Texture;
 
 	/*
-	* Management runtime generated GPU Textures ro Buffers
+	* Management runtime generated GPU Textures
 	*/
 	class RenderResourceRegistry
 	{
 	public:
-		RenderResourceRegistry() noexcept;
-		
+		struct CreateInfo
+		{
+			RGGpuResourceAllocator* m_GpuResourceAllocator = nullptr;
+			RGExternalResourceRegistry* m_ExternalResourceRegistry = nullptr;
+		};
+
+		enum class TextureIndex : uint8_t
+		{
+			IBL_BrdfLut,
+
+			Count
+		};
+
+	private:
+		struct TextureEntry
+		{
+			RGTextureDesc m_RgTexDesc{};
+			ResourceIndex m_InternalIndex{};
+			ResourceIndex m_ExternalIndex{};
+			uint32_t m_BindlessSrvIndex = 0;
+
+			bool m_Allocated = false;
+			bool m_Dirty = false;
+		};
+
+	public:
+		explicit RenderResourceRegistry(const CreateInfo& createInfo) noexcept;
+		GGLAB_DELETE_COPYABLE_MOVABLE(RenderResourceRegistry);
+		~RenderResourceRegistry() = default;
+
+		void MarkDirty(TextureIndex index) noexcept;
+		bool IsDirty(TextureIndex index) const noexcept;
+		void ClearDirty(TextureIndex index) noexcept;
+
+		DX12Texture* GetTexture(TextureIndex index) noexcept;
+		ResourceIndex GetEnternalIndex(TextureIndex index) const noexcept;
+		uint32_t GetBindlessSrvIndex(TextureIndex index) const noexcept;
+
+		void ReleaseAll(const DX12FencePoint& fencePoint) noexcept;
+
+	private:
+		RGGpuResourceAllocator* m_GpuResourceAllocator = nullptr;
+		RGExternalResourceRegistry* m_ExternalResourceRegistry = nullptr;
+
+		std::array<TextureEntry, utils::EnumCount<TextureIndex>()> m_TextureEntries;
 	};
 }
