33d81c4a23d1f3d158ff46b932d30a61ee2bba89
ruisong <airiness021@gmail.com>
2026-05-15 00:14:51 +0900

    Fixed ignored issue.

---
+++ Sources/Core/Log/Logger.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <cstdint>
+#include <array>
+#include <memory>
+#include <spdlog/spdlog.h>
+
+namespace gglab
+{
+	class Logger
+	{
+	public:
+		enum class LoggerType : uint32_t
+		{
+			Application,
+			Graphics,
+
+			Count
+		};
+	public:
+		static void Initialize() noexcept;
+
+		static std::shared_ptr<spdlog::logger>& GetLogger(LoggerType type) noexcept;
+
+	private:
+		static std::array<std::shared_ptr<spdlog::logger>, static_cast<uint32_t>(LoggerType::Count)> s_Loggers;
+	};
+}
