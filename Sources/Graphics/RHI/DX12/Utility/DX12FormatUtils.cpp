ad81ce14bb67487d37d6785b1dfcc32191dd2702
ruisong <airiness021@gmail.com>
2026-06-30 00:42:23 +0900

    RHIForamt and dxgi format meta info.

--- Sources/Graphics/RHI/DX12/Utility/DX12FormatUtils.cpp
+++ Sources/Graphics/RHI/DX12/Utility/DX12FormatUtils.cpp
@@ -1,80 +1,66 @@
 #include "Core/Precompiled.h"
 #include "Graphics/RHI/DX12/Utility/DX12FormatUtils.h"
 
+#include <array>
+
 namespace gglab
 {
+	namespace
+	{
+		constexpr std::array<DXGI_FORMAT, static_cast<size_t>(RHIFormat::Count)> DXGIFormats =
+		{
+			DXGI_FORMAT_UNKNOWN,
+			DXGI_FORMAT_R8G8B8A8_TYPELESS,
+			DXGI_FORMAT_R8G8B8A8_UNORM,
+			DXGI_FORMAT_R8G8B8A8_UNORM_SRGB,
+			DXGI_FORMAT_R16G16_FLOAT,
+			DXGI_FORMAT_R16G16B16A16_FLOAT,
+			DXGI_FORMAT_R32G32_FLOAT,
+			DXGI_FORMAT_R32G32B32_FLOAT,
+			DXGI_FORMAT_R32G32B32A32_FLOAT,
+			DXGI_FORMAT_R32_TYPELESS,
+			DXGI_FORMAT_R32_FLOAT,
+			DXGI_FORMAT_R32_UINT,
+			DXGI_FORMAT_D24_UNORM_S8_UINT,
+			DXGI_FORMAT_D32_FLOAT,
+		};
+
+		static_assert([]
+			{
+				for (size_t index = 1; index < DXGIFormats.size(); ++index)
+				{
+					if (DXGIFormats[index] == DXGI_FORMAT_UNKNOWN)
+					{
+						return false;
+					}
+				}
+				return DXGIFormats[0] == DXGI_FORMAT_UNKNOWN;
+			}(), "Every RHIFormat must have a DXGI_FORMAT mapping.");
+	}
+
 	DXGI_FORMAT ToDXGIFormat(RHIFormat format) noexcept
 	{
-		switch (format)
+		const size_t index = static_cast<size_t>(format);
+		if (index < DXGIFormats.size())
 		{
-		case RHIFormat::Unknown:
-			return DXGI_FORMAT_UNKNOWN;
-		case RHIFormat::R8G8B8A8Typeless:
-			return DXGI_FORMAT_R8G8B8A8_TYPELESS;
-		case RHIFormat::R8G8B8A8Unorm:
-			return DXGI_FORMAT_R8G8B8A8_UNORM;
-		case RHIFormat::R8G8B8A8UnormSrgb:
-			return DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
-		case RHIFormat::R16G16Float:
-			return DXGI_FORMAT_R16G16_FLOAT;
-		case RHIFormat::R16G16B16A16Float:
-			return DXGI_FORMAT_R16G16B16A16_FLOAT;
-		case RHIFormat::R32G32Float:
-			return DXGI_FORMAT_R32G32_FLOAT;
-		case RHIFormat::R32G32B32Float:
-			return DXGI_FORMAT_R32G32B32_FLOAT;
-		case RHIFormat::R32G32B32A32Float:
-			return DXGI_FORMAT_R32G32B32A32_FLOAT;
-		case RHIFormat::R32Typeless:
-			return DXGI_FORMAT_R32_TYPELESS;
-		case RHIFormat::R32Float:
-			return DXGI_FORMAT_R32_FLOAT;
-		case RHIFormat::R32Uint:
-			return DXGI_FORMAT_R32_UINT;
-		case RHIFormat::D24UnormS8Uint:
-			return DXGI_FORMAT_D24_UNORM_S8_UINT;
-		case RHIFormat::D32Float:
-			return DXGI_FORMAT_D32_FLOAT;
+			return DXGIFormats[index];
 		}
 
-		GGLAB_UNREACHABLE("Unhandled RHIFormat.");
+		GGLAB_ASSERT_MSG(false, "Unhandled RHIFormat.");
+		return DXGI_FORMAT_UNKNOWN;
 	}
 
 	RHIFormat ToRHIFormat(DXGI_FORMAT format) noexcept
 	{
-		switch (format)
+		for (size_t index = 0; index < DXGIFormats.size(); ++index)
 		{
-		case DXGI_FORMAT_UNKNOWN:
-			return RHIFormat::Unknown;
-		case DXGI_FORMAT_R8G8B8A8_TYPELESS:
-			return RHIFormat::R8G8B8A8Typeless;
-		case DXGI_FORMAT_R8G8B8A8_UNORM:
-			return RHIFormat::R8G8B8A8Unorm;
-		case DXGI_FORMAT_R8G8B8A8_UNORM_SRGB:
-			return RHIFormat::R8G8B8A8UnormSrgb;
-		case DXGI_FORMAT_R16G16_FLOAT:
-			return RHIFormat::R16G16Float;
-		case DXGI_FORMAT_R16G16B16A16_FLOAT:
-			return RHIFormat::R16G16B16A16Float;
-		case DXGI_FORMAT_R32G32_FLOAT:
-			return RHIFormat::R32G32Float;
-		case DXGI_FORMAT_R32G32B32_FLOAT:
-			return RHIFormat::R32G32B32Float;
-		case DXGI_FORMAT_R32G32B32A32_FLOAT:
-			return RHIFormat::R32G32B32A32Float;
-		case DXGI_FORMAT_R32_TYPELESS:
-			return RHIFormat::R32Typeless;
-		case DXGI_FORMAT_R32_FLOAT:
-			return RHIFormat::R32Float;
-		case DXGI_FORMAT_R32_UINT:
-			return RHIFormat::R32Uint;
-		case DXGI_FORMAT_D24_UNORM_S8_UINT:
-			return RHIFormat::D24UnormS8Uint;
-		case DXGI_FORMAT_D32_FLOAT:
-			return RHIFormat::D32Float;
-		default:
-			GGLAB_ASSERT_MSG(false, "Unsupported DXGI_FORMAT for RHIFormat conversion.");
-			return RHIFormat::Unknown;
+			if (DXGIFormats[index] == format)
+			{
+				return static_cast<RHIFormat>(index);
+			}
 		}
+
+		GGLAB_ASSERT_MSG(false, "Unsupported DXGI_FORMAT for RHIFormat conversion.");
+		return RHIFormat::Unknown;
 	}
 }
