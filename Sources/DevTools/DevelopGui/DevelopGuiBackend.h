de8c7e41fa117ac18ae777c1281c5cc4cfa648dc
ruisong <airiness021@gmail.com>
2026-07-04 22:35:14 +0900

    Resolve issue
    Fix DevTool draw still be called when DevelopGuiBackend failed be created issue.

--- Sources/DevTools/DevelopGui/DevelopGuiBackend.h
+++ Sources/DevTools/DevelopGui/DevelopGuiBackend.h
@@ -12,6 +12,13 @@ namespace gglab
 	class DevelopGuiBackend
 	{
 	public:
+		enum class State : uint8_t
+		{
+			Inactive,
+			Active,
+			FrameOpen,
+		};
+
 		struct CreateInfo
 		{
 			void* m_NativeWindowHandle = nullptr;
@@ -23,14 +30,19 @@ namespace gglab
 		GGLAB_DELETE_COPYABLE_MOVABLE(DevelopGuiBackend);
 		virtual ~DevelopGuiBackend() = default;
 
-		virtual void Initialize(const CreateInfo& createInfo) noexcept = 0;
+		[[nodiscard]] virtual bool Initialize(const CreateInfo& createInfo) noexcept = 0;
 		virtual void Finalize() noexcept = 0;
 
-		virtual void NewFrame() noexcept = 0;
+		[[nodiscard]] virtual bool NewFrame() noexcept = 0;
 		virtual void RenderDrawData(
 			RHIGraphicsCommandContext* commandContext,
 			RHITextureViewHandle renderTarget) noexcept = 0;
 		virtual void EndFrame() noexcept = 0;
+
+		[[nodiscard]] virtual State GetState() const noexcept = 0;
+		[[nodiscard]] bool IsActive() const noexcept { return GetState() != State::Inactive; }
+		[[nodiscard]] bool IsFrameOpen() const noexcept { return GetState() == State::FrameOpen; }
+
 		[[nodiscard]] virtual ImTextureID ResolveTextureId(
 			RHIDescriptorHandle descriptor) const noexcept = 0;
 	};
