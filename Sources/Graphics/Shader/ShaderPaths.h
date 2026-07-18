8b58bd5d6fd88cdbd90add224b45b864b71d53c3
airiness <airiness021@gmail.com>
2026-07-18 14:22:05 +0900

    Change Shader dir, Edit junction target file.

--- Sources/Graphics/Shader/ShaderPaths.h
+++ Sources/Graphics/Shader/ShaderPaths.h
@@ -5,7 +5,7 @@
 namespace gglab
 {
 	// Build deployment must expose this directory relative to the executable output.
-	inline constexpr wchar_t ShaderSourceRelativeDirectory[] = L"Assets/Shaders";
+	inline constexpr wchar_t ShaderSourceRelativeDirectory[] = L"Shaders";
 
 	[[nodiscard]] std::filesystem::path GetShaderSourceRoot() noexcept;
 }
