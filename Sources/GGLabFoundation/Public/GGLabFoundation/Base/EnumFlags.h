b811f6c6c44141142c6872d87ce56d5277354939
airiness <airiness021@gmail.com>
2026-08-20 19:27:56 +0900

    refactor(format): code format refactor

--- Sources/GGLabFoundation/Public/GGLabFoundation/Base/EnumFlags.h
+++ Sources/GGLabFoundation/Public/GGLabFoundation/Base/EnumFlags.h
@@ -4,29 +4,29 @@
 
 namespace gglab
 {
-#define GGLAB_ENUM_FLAGS(EnumType)                                                  \
-constexpr inline EnumType operator|(EnumType a, EnumType b) noexcept {              \
-	using U = std::underlying_type_t<EnumType>;                                       \
-	return static_cast<EnumType>(static_cast<U>(a) | static_cast<U>(b));              \
-}                                                                                   \
-constexpr inline EnumType operator&(EnumType a, EnumType b) noexcept {              \
-	using U = std::underlying_type_t<EnumType>;                                       \
-	return static_cast<EnumType>(static_cast<U>(a) & static_cast<U>(b));              \
-}                                                                                   \
-constexpr inline EnumType& operator|=(EnumType& a, EnumType b) noexcept {           \
-	a = (a | b);                                                                      \
-	return a;                                                                         \
-}                                                                                   \
-constexpr inline EnumType& operator&=(EnumType& a, EnumType b) noexcept {           \
-	a = (a & b);                                                                      \
-	return a;                                                                         \
-}                                                                                   \
-constexpr inline bool Any(EnumType v) noexcept {                                    \
-	using U = std::underlying_type_t<EnumType>;                                       \
-	return static_cast<U>(v) != 0;                                                    \
-}                                                                                   \
-constexpr inline bool Test(EnumType v, EnumType t) noexcept {                       \
-	using U = std::underlying_type_t<EnumType>;                                       \
-	return (static_cast<U>(v) & static_cast<U>(t)) != 0;                              \
+#define GGLAB_ENUM_FLAGS(EnumType)											\
+constexpr inline EnumType operator|(EnumType a, EnumType b) noexcept {		\
+	using U = std::underlying_type_t<EnumType>;								\
+	return static_cast<EnumType>(static_cast<U>(a) | static_cast<U>(b));	\
+}																			\
+constexpr inline EnumType operator&(EnumType a, EnumType b) noexcept {		\
+	using U = std::underlying_type_t<EnumType>;								\
+	return static_cast<EnumType>(static_cast<U>(a) & static_cast<U>(b));	\
+}																			\
+constexpr inline EnumType& operator|=(EnumType& a, EnumType b) noexcept {	\
+	a = (a | b);															\
+	return a;																\
+}																			\
+constexpr inline EnumType& operator&=(EnumType& a, EnumType b) noexcept {	\
+	a = (a & b);															\
+	return a;																\
+}																			\
+constexpr inline bool Any(EnumType v) noexcept {							\
+	using U = std::underlying_type_t<EnumType>;								\
+	return static_cast<U>(v) != 0;											\
+}																			\
+constexpr inline bool Test(EnumType v, EnumType t) noexcept {				\
+	using U = std::underlying_type_t<EnumType>;								\
+	return (static_cast<U>(v) & static_cast<U>(t)) != 0;					\
 }
 }
